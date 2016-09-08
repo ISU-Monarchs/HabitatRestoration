13acbb64e1795726754f0f92f6be17efd536e237
jarad <niemi@iastate.edu>
2016-09-08 10:54:02 -0500

    Add compiled nectar data set

---
+++ R/data.R
@@ -0,0 +1,19 @@
+#' Nectar plant species
+#'
+#' This data set contains the count of the number of plants observed within certain distances of a transect.
+#'
+#' @format A data frame with the following variables:
+#' \itemize{
+#'   \item species: common name for nectar plant species
+#'   \item year: year of observation
+#'   \item month: month of observation
+#'   \item day: day of observation
+#'   \item recorder: individual who recorded the observations
+#'   \item site: location of observation
+#'   \item transect: id of the transect
+#'   \item round: the round number
+#'   \item distance: the interval of the observation
+#'   \item count: number of plants observed
+#' }
+#' @format A data frame with the variables above.
+"nectar"
--- data-raw/nectar.R
+++ data-raw/nectar.R
@@ -20,12 +20,27 @@ read_dir = function(path, pattern, into) {
 ###########################################################
 
 nectar = read_dir(path = ".",
-             pattern = "*.csv",
-             into = c("na","type",
-               "year","month","day","recorder",
-               "site","transect","round",
-               "extension")) %>%
-  select(-na,-extension)
+                  pattern = "*.csv",
+                  into = c("na","type",
+                           "year","month","day","recorder",
+                           "site","transect","round",
+                           "extension")) %>%
+  select(-na,-type,-extension) %>%             # Might want to include type in the future
+
+  rename(species = `Nectar Plant Species`) %>%
+
+  gather(distance, count,
+         -species, -year, -month, -day, -recorder, -site, -transect, -round,
+         na.rm=TRUE)
+
+#   # The above gather should really explicitly gather the distances, but the code below doesn't work
+#   # because the gather does not recognize the column names.
+#   #
+#   # Convert from wide to long format for distances
+#   gather_(key_col     = "distance",
+#           value_col   = "count",
+#           gather_cols = c("0-19m", "20-39m", "40-50m", "60-79m", "80-100m"),
+#           na.rm       = TRUE) %>%
 
 devtools::use_data(nectar,
                    overwrite = TRUE)
