b865187f10acef086f5a8742d2d6c4a703b835cb
Jarad Niemi <niemi@iastate.edu>
2016-09-08 12:21:44 -0500

    Add a couple example tests

---
+++ tests/testthat/test-count.R
@@ -0,0 +1,12 @@
+context("count")
+
+test_that("count is non-negative",
+					expect_true(all(nectar$count>=0)))
+
+test_that("count is not too large",
+					expect_true(all(nectar$count<501)))
+
+context("recorder")
+
+test_that("recorder exists",
+					expect_true(all(nectar$recorder %in% c("Seth","Teresa"))))
