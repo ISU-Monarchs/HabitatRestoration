d2fa3706f58ea6af3ae8f3922ad60fdc37586ee8
jarad <niemi@iastate.edu>
2016-09-08 10:23:39 -0500

    Add compiled nectar data to package

---
+++ data-raw/nectar.R
@@ -0,0 +1,30 @@
+require(dplyr)
+require(tidyr)
+
+my_read_csv = function(f, into) {
+  readr::read_csv(f) %>%
+    mutate(file=f) %>%
+    separate(file, into)
+}
+
+read_dir = function(path, pattern, into) {
+  files = list.files(path = path,
+                     pattern = pattern,
+                     recursive = TRUE,
+                     full.names = TRUE)
+  plyr::ldply(files, my_read_csv, into = into)
+}
+
+# Above taken from https://gist.github.com/jarad/8f3b79b33489828ab8244e82a4a0c5b3
+
+###########################################################
+
+nectar = read_dir(path = ".",
+             pattern = "*.csv",
+             into = c("na","type",
+               "year","month","day","recorder",
+               "site","transect","round",
+               "extension")) %>%
+  select(-na,-extension)
+
+devtools::use_data(nectar)
