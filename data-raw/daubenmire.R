9300dbe26f7b33f79cc36ddada2ae685b456a526
Jarad Niemi <niemi@iastate.edu>
2017-03-15 11:33:11 -0500

    Start updating daubenmire data

---
+++ data-raw/daubenmire.R
@@ -0,0 +1,52 @@
+library(dplyr)
+library(tidyr)
+library(readr)
+
+my_read_csv = function(f, into) {
+  readr::read_csv(f, col_types = cols(.default = "c")) %>%
+    mutate(file=f) %>%
+    separate(file, into)
+}
+
+read_dir = function(path, pattern, into) {
+  files = list.files(path = path,
+                     pattern = pattern,
+                     recursive = TRUE,
+                     full.names = TRUE)
+  plyr::ldply(files, my_read_csv, into = into)
+}
+
+# Above taken from https://gist.github.com/jarad/8f3b79b33489828ab8244e82a4a0c5b3
+
+###########################################################
+
+daubenmire = read_dir(path = "daubenmire",
+                  pattern = "*.csv",
+                  into = c("daubenmire",
+                           "year","month","day","recorder",
+                           "site","transect","round",
+                           "extension")) %>%
+  select(-daubenmire, -extension) %>%             # Might want to include type in the future
+
+  # rename(daubenmire_plant_species = `daubenmire Plant Species`) %>%
+
+  gather(distance, count,
+         -`daubenmire Plant Species`, 
+         -year, -month, -day, 
+         -recorder, -site, -transect, -round,
+         na.rm = TRUE) %>% 
+	
+	mutate(count = as.numeric(count))            # some columns are character
+	
+
+#   # The above gather should really explicitly gather the distances, but the code below doesn't work
+#   # because the gather does not recognize the column names.
+#   #
+#   # Convert from wide to long format for distances
+#   gather_(key_col     = "distance",
+#           value_col   = "count",
+#           gather_cols = c("0-19m", "20-39m", "40-50m", "60-79m", "80-100m"),
+#           na.rm       = TRUE) %>%
+
+devtools::use_data(daubenmire,
+                   overwrite = TRUE)
