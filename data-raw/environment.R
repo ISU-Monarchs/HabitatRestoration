ed2ee715ba2cd94c889e6581ee55c4426da466f4
Jarad Niemi <niemi@iastate.edu>
2018-04-06 08:55:52 -0500

    Add script to read in environment data

---
+++ data-raw/environment.R
@@ -0,0 +1,48 @@
+library("dplyr")
+library("tidyr")
+
+my_read_csv = function(f, into) {
+  cat("Reading",f,"\n")
+  readr::read_csv(f, 
+                  col_names = c("variable","value"),
+                  col_types = "cc") %>%
+    mutate(file=f) %>%
+    separate(file, into)
+}
+
+read_dir = function(path, pattern, into) {
+  files = list.files(path = path,
+                     pattern = pattern,
+                     recursive = TRUE,
+                     full.names = TRUE)
+  plyr::ldply(files, my_read_csv, into = into)
+}
+
+# Above taken from https://gist.github.com/jarad/8f3b79b33489828ab8244e82a4a0c5b3
+
+###########################################################
+
+environment <- read_dir(path = "environment",
+               pattern = "*.csv",
+               into = c("environment",
+                        "year","month","day","recorder",
+                        "site","transect","round",
+                        "extension")) %>%
+  select(-environment, -extension) %>%
+  tidyr::spread(variable, value)
+
+names(environment) <- gsub('“', '', names(environment))
+names(environment) <- gsub('”', '', names(environment))
+
+environment <- environment %>%
+  mutate(year           = as.numeric(year),
+         month          = as.numeric(month),
+         day            = as.numeric(day),
+         milkweed_ramet = as.numeric(milkweed_ramet),
+         temperature    = as.numeric(temperature),
+         
+         currently_flowering_plants = factor(currently_flowering_plants,
+                                             levels = c("0","0-5","5-25","25-50","50-75")))
+
+devtools::use_data(environment,
+                   overwrite = TRUE)
