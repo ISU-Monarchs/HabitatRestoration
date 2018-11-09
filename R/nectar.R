d571abad2a97874a3c627ed4d9fbb0bf663d1e8e
Appelgate <sethapp@iastate.edu>
2018-11-09 10:02:28 -0600

    Update text and clarify. nectar.R helpfile

--- R/nectar.R
+++ R/nectar.R
@@ -1,19 +1,19 @@
 #' Nectar plant species
 #'
-#' This data set contains the count of the number of plants observed within certain distances of a transect.
+#' This data set contains the count of the number of plant species specific floral units observed within certain sections of a transect.
 #'
 #' @format A data frame with the following variables:
 #' \itemize{
-#'   \item species: common name for nectar plant species
+#'   \item Nectar Plant Species: common name for nectar plant species
 #'   \item year: year of observation
 #'   \item month: month of observation
 #'   \item day: day of observation
 #'   \item recorder: individual who recorded the observations
-#'   \item site: location of observation
-#'   \item transect: id of the transect
-#'   \item round: the round number
-#'   \item distance: the interval of the observation
-#'   \item count: number of plants observed
+#'   \item siteID: id of the site
+#'   \item transectID: id of the transect
+#'   \item round: the round number: 1, 2, or 3
+#'   \item distance: the interval of the observation in length of meters along transect
+#'   \item count: number of plant species specific floral units observed
 #' }
 #' @format A data frame with the variables above.
 "nectar"
