0520a403efc78bca853f9caeade862a3bfa9d68b
Appelgate <sethapp@iastate.edu>
2018-11-09 14:25:59 -0600

    create new landscape.R helpfile

---
+++ R/landscape.R
@@ -0,0 +1,18 @@
+#' Landscape
+#'
+#' This data set contains general information about the landscape surrounding the plots
+#' Data set was only used in 2016. Replaced by 'environment'
+#'
+#' @format A data frame with the following variables:
+#' \itemize{
+#'   \item landscape: contains general information about landscape surrounding plots. Yes/no answers to milkweed presence, flowering plants, and habitat type 
+#'   \item year: year of observation
+#'   \item month: month of observation
+#'   \item day: day of observation
+#'   \item recorder: individual who observed (and possibly recorded) the observations
+#'   \item siteID: id of the site
+#'   \item transectID: id of the transect
+#'   \item round: the round number: 1, 2, or 3
+#' }
+#' @format A data frame with the variables above.
+"landscape"
