920ce8be8beecd3af61c78cd6f365c6f99ef9dcf
Appelgate <sethapp@iastate.edu>
2018-11-09 12:16:07 -0600

    add new daubenmire.R helpfile

---
+++ R/daubenmire.R
@@ -0,0 +1,21 @@
+#' Daubenmire
+#'
+#' This data set contains daubenmire land cover %'s, count data for 3 milkweed species, and litter depth in cm observed at 10m intervals inside a 0.5m square along a transect.
+#'
+#' @format A data frame with the following variables:
+#' \itemize{
+#'   \item daubenmire: daubenmire land cover %'s, count data for 3 milkweed species, and litter depth in cm observed at 10m intervals inside a 0.5m square
+#'   \item year: year of observation
+#'   \item month: month of observation
+#'   \item day: day of observation
+#'   \item recorder: individual who observed (and possibly recorded) the observations
+#'   \item siteID: id of the site
+#'   \item transectID: id of the transect
+#'   \item round: the round number: 1, 2, or 3
+#'   \item distance: the interval of the observation in length of meters along transect
+#'   \item landcover
+#'   \item percentage
+#'   \item section
+#' }
+#' @format A data frame with the variables above.
+"daubenmire"
