8ea7c877a05d7b05d8a44e90e17c9d1506cb1fd3
Appelgate <sethapp@iastate.edu>
2018-11-09 11:40:27 -0600

    clarify wording. nectar.R helpfile

--- R/nectar.R
+++ R/nectar.R
@@ -4,11 +4,11 @@
 #'
 #' @format A data frame with the following variables:
 #' \itemize{
-#'   \item Nectar Plant Species: common name for nectar plant species
+#'   \item nectar: common name for nectar plant species
 #'   \item year: year of observation
 #'   \item month: month of observation
 #'   \item day: day of observation
-#'   \item recorder: individual who recorded the observations
+#'   \item recorder: individual who observed (and possibly recorded) the observations
 #'   \item siteID: id of the site
 #'   \item transectID: id of the transect
 #'   \item round: the round number: 1, 2, or 3
