23485f3eecb08940a9ddf022293b4f450d8de1ab
Appelgate <sethapp@iastate.edu>
2018-11-09 11:38:43 -0600

    add new help file. bee.R

---
+++ R/bee.R
@@ -0,0 +1,20 @@
+#' Bee species and nectar plant species
+#'
+#' This data set contains the count of bees observed and the specific flowering plant they were nectaring on observed within certain sections of a transect.
+#'
+#' @format A data frame with the following variables:
+#' \itemize{
+#'   \item bee: type of bee: honey bee, bumble bee, or solitary bee
+#'   \item year: year of observation
+#'   \item month: month of observation
+#'   \item day: day of observation
+#'   \item recorder: individual who observed (and possibly recorded) the observations
+#'   \item siteID: id of the site
+#'   \item transectID: id of the transect
+#'   \item round: the round number: 1, 2, or 3
+#'   \item distance: the interval of the observation in length of meters along transect
+#'   \item count: number of bees observed for each bee type
+#'   \item nectar: common name for nectar plant species
+#' }
+#' @format A data frame with the variables above.
+"bee"
