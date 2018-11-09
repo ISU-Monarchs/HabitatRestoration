6e20f200d1ceff24d97865fca3d5827408dffce1
Appelgate <sethapp@iastate.edu>
2018-11-09 14:13:40 -0600

    add new monarch.R helpfile

---
+++ R/monarch.R
@@ -0,0 +1,22 @@
+#' Monarch
+#'
+#' This data set contains count data for monarch adults, eggs, and instars as well as count data for palmer amaranth.
+#'
+#' @format A data frame with the following variables:
+#' \itemize{
+#'   \item monarch: count data for monarch adults, eggs, and instars. count data for palmer amaranth
+#'   \item year: year of observation
+#'   \item month: month of observation
+#'   \item day: day of observation
+#'   \item recorder: individual who observed (and possibly recorded) the observations
+#'   \item siteID: id of the site
+#'   \item transectID: id of the transect
+#'   \item round: the round number: 1, 2, or 3
+#'   \item start_time: time survey began
+#'   \item end_time: time survey ended
+#'   \item wind: wind in miles per hour at start of survey
+#'   \item sky: cloud cover (clear, mostly clear, mostly cloudy, cloudy, rain)
+#'   \item temp: temperature in fahrenheit at start of survey
+#' }
+#' @format A data frame with the variables above.
+"monarch"
