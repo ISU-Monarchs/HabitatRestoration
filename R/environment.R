7aba5663ea6cef9d36c3c165c3d840b5c7d95dd6
Appelgate <sethapp@iastate.edu>
2018-11-12 14:23:24 -0600

    update variables in environment.R helpfile

--- R/environment.R
+++ R/environment.R
@@ -5,7 +5,6 @@
 #'
 #' @format A data frame with the following variables:
 #' \itemize{
-#'   \item environment: weather data and floral/milkweed resources of surrounding landscape
 #'   \item year: year of observation
 #'   \item month: month of observation
 #'   \item day: day of observation
@@ -13,12 +12,12 @@
 #'   \item siteID: id of the site
 #'   \item transectID: id of the transect
 #'   \item round: the round number: 1, 2, or 3
-#'   \item milkweed_ramet: count of milkweed ramets in defined area surrounding plot
-#'   \item temperature: temperature in fahrenheit
-#'   \item wind: wind speed in miles per hour
-#'   \item sky: cloud cover group (clear, mostly clear, mostly cloudy, cloudy, rain)
 #'   \item currently_flowering_plants: daubenmire style land cover of currently flowering plants in defined area surrounding plot. Grouped by percentages: 0,0-5,5-25,25-50,50-75,75-100
 #'   \item dominant_flowering_species: dominant flowering plant species in defined area surrounding plot
+#'   \item milkweed_ramet: count of milkweed ramets in defined area surrounding plot
+#'   \item sky: cloud cover group name(clear, mostly clear, mostly cloudy, cloudy, rain)
+#'   \item temperature: temperature in fahrenheit
+#'   \item wind: wind speed in miles per hour
 #' }
 #' @format A data frame with the variables above.
 "environment"
