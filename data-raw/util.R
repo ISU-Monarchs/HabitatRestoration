886f1e2fc19f7267b5a3d61d10d366980006a482
Jarad Niemi <niemi@iastate.edu>
2018-11-16 09:47:27 -0600

    Add script to find missing files

--- data-raw/util.R
+++ data-raw/util.R
@@ -1,5 +1,7 @@
 stop("This file is not meant to be sourced. Run lines manually.")
 
+# The scripts below are meant to be run from the data-raw/ directory
+
 library("tidyverse")
 
 ################################################################################
@@ -22,10 +24,12 @@ system('git commit -m "Rename *.PDF to *.pdf"')
 
 ################################################################################
 ## Find missing csv,csv2, or pdf files
+
+# $ is needed because the daubenmire directory has csv and csv2 files
 d <- read.table(textConnection("
 type dir extension
 bee bee .csv
-daubenmire daubenmire .csv
+daubenmire daubenmire .csv$
 environment environment .csv
 landscape2016 landscape2016 .csv
 monarch monarch .csv
@@ -39,8 +43,17 @@ find_missing <- function(d) {
   pdf_files <- gsub(".pdf", "", list.files(path = dir, pattern = ".pdf", full.names = TRUE, recursive = TRUE))
   csv_files <- gsub(ext,    "", list.files(path = dir, pattern = ext,    full.names = TRUE, recursive = TRUE))
   
-  bind_rows(missing="pdf", setdiff(csv_files, pdf_files),
-            missing="csv", setdiff(pdf_files, csv_files))
+  pdf_missing <- paste0(setdiff(csv_files, pdf_files), ".pdf")
+  csv_missing <- paste0(setdiff(pdf_files, csv_files), ext)
+  
+  data_frame(missing = c(pdf_missing,csv_missing))
 }
 
+d %>% 
+  group_by(type) %>%
+  do(find_missing(.)) %>%
+  ungroup() %>%
+  select(-type) %>%
+  mutate(missing = gsub("\\$","",missing)) %>%
+  write_csv(path="missing_files.csv")
 ################################################################################
