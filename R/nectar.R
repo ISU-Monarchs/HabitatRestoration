1a89b803e2d8b18dba3b31d9000827e08eb32f8e
Appelgate <sethapp@iastate.edu>
2019-02-07 16:00:43 -0600

    update nectar helpfile

--- R/nectar.R
+++ R/nectar.R
@@ -4,14 +4,14 @@
 #'
 #' @format A data frame with the following variables:
 #' \itemize{
-#'   \item Nectar Plant Species: common name for nectar plant species which were flowering at the time of the survey
 #'   \item year: year of observation
 #'   \item month: month of observation
 #'   \item day: day of observation
-#'   \item recorder: individual who observed (and possibly recorded) the observations
 #'   \item siteID: id of the site
 #'   \item transectID: id of the transect
 #'   \item round: the round number: 1, 2, or 3
+#'   \item section: the interval of the observation in length of meters along transect
+#'   \item Nectar Plant Species: common name for nectar plant species which were flowering at the time of the survey
 #'   \item count: number of plant species specific floral units observed
 #' }
 #' @format A data frame with the variables above.
