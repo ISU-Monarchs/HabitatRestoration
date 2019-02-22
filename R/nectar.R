55975f47f80939c306610a2b8537de433b3ca7af
Appelgate <sethapp@iastate.edu>
2019-02-22 11:08:55 -0600

    update nectar R helpfile

--- R/nectar.R
+++ R/nectar.R
@@ -3,9 +3,15 @@
 #' This data set contains the count of the number of plant species specific 
 #' floral units observed within certain sections of a transect.
 #'
-#' @section NewSection: test
+#' @section Data Collection Protocol
 #' 
-#' Here is some information.
+#' Nectar plants were surveyed by an observer walking along a permanent transect line. 
+#' Observer walked a line parallel to the transect at a length of 1.5m to the right of the transect. 
+#' Observer visually surveyed a 1m wide band along the whole length of the transect, recording every visual detection of a blooming floral resource.
+#' Area surveyed is the same as the length of transect because width of survey was 1m along whole length of transect.
+#' Transect lengths can be found in 'survey' folder.
+#' 
+#' 
 #'
 #' @format A data frame with the following variables:
 #' \itemize{
