145915634f6fb6f1f1e4d98475110fe5b823d4f1
Appelgate <sethapp@iastate.edu>
2019-02-22 15:10:08 -0600

    update landscape R helpfile

--- R/landscape.R
+++ R/landscape.R
@@ -2,6 +2,13 @@
 #'
 #' This data set contains general information about the landscape surrounding the plots
 #' Data set was only used in 2016. Replaced by 'environment'
+#' 
+#' @section Data Collection Protocol
+#' 
+#' Landscape data only exists for 2016. In 2017 and following years, data collection relating to the outside of the plot is stored in the 'environment' data set. 
+#' Surveyed the area outside the perimeter of the plot to determine if milkweed and flowering plants were present. 
+#' Physical data sheet also listed ‘type of habitat’ which was outside the plot but this data was too ambiguous to be used so it was not recorded in the Git ‘landscape’ data set. 
+#' Plot perimeter was not established until round 3 2016 data collection. Round 1/2 landscape surveyed area was at the discretion of the observer as to what was ‘outside’ the plot.
 #'
 #' @format A data frame with the following variables:
 #' \itemize{
