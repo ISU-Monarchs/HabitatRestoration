1030f23f9de090454c06db08bcf671da04efbd65
Appelgate <sethapp@iastate.edu>
2019-02-22 14:00:11 -0600

    update nectar R helpfile

--- R/nectar.R
+++ R/nectar.R
@@ -7,11 +7,24 @@
 #' 
 #' Nectar plants were surveyed by an observer walking along a permanent transect line. 
 #' Observer walked a line parallel to the transect at a length of 1.5m to the right of the transect. 
-#' Observer visually surveyed a 1m wide band along the whole length of the transect, recording every visual detection of a blooming floral resource.
-#' Area surveyed is the same as the length of transect because width of survey was 1m along whole length of transect.
+#' Observer visually surveyed a 1m wide band along the whole length of the transect, recording every visual detection of an actively flowering floral resource.
+#' Area surveyed was the same as the length of transect because width of survey was 1m along whole length of transect.
 #' Transect lengths can be found in 'survey' folder.
+#' ‘nectar_species_scientific_name_counting_unit’ document contains information which denotes species, specific counting units, and other plant information. 
+#' File is located in the folder 'data-raw/nectar' in the Git repository ISUmonarch.
+#' 
+#' Observer also recorded every visual detection of a common milkweed, swamp milkweed, or butterfly milkweed ramet.
+#' Milkweed ramet was recorded regardless of whether or not it was flowering. 
+#' Data allows for calculation of milkweed density for these three species.
 #' 
 #' 
+#' @section ‘nectar_species_scientific_name_counting_unit’ document explanation
+#' ‘common_name’ 'genus' 'species' common names and corresponding scientific names
+#' 'field count unit' specific to each species. Counting units are either umbel, head, ramet, or spike
+#' 'native_introduced_both' classification of plants as defined by the USDA PLANTS database.
+#' 'planted_non_planted' Indicating which species were in the native seed mix planted. Seed mix same at all plots.
+#' 'technical_count' more specifc description of 'field_count_unit'
+#' 'inflorescence_name_when_technical' details if inflorescence is not easily explained
 #'
 #' @format A data frame with the following variables:
 #' \itemize{
