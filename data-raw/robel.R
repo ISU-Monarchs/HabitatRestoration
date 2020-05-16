f7c818a2dfaaf5043fb30b1e1daf6712e24fd176
Jarad Niemi <niemi@iastate.edu>
2020-05-16 13:35:45 -0500

    Update robel processing script to include sections
    related to #96

--- data-raw/robel.R
+++ data-raw/robel.R
@@ -1,59 +1,75 @@
-library("dplyr")
-library("tidyr")
-library("readr")
+source("common.R")
 
-my_read_csv = function(f, into) {
-  cat("Reading",f,"\n")
+read_robel_csv = function(f, into) {
+  # cat("Reading",f,"\n")
   readr::read_csv(f, 
                   col_types = cols(.default = "c")) %>%
     mutate(file=f) %>%
     separate(file, into)
 }
 
-read_dir = function(path, pattern, into) {
-  files = list.files(path = path,
-                     pattern = pattern,
-                     recursive = TRUE,
-                     full.names = TRUE)
-  plyr::ldply(files, my_read_csv, into = into)
-}
-
-# Above taken from https://gist.github.com/jarad/8f3b79b33489828ab8244e82a4a0c5b3
-
 ###########################################################
 
-robel = read_dir(path = "daubenmire",
-                  pattern = "csv2",
-                  into = c("robel",
-                           "year","month","day","observer",
-                           "siteID","transectID","round",
-                           "extension")) %>%
-  select(-robel, -extension) %>%
+robel_files = list.files(path = "daubenmire",
+                              pattern = "csv2",
+                              recursive = TRUE,
+                              full.names = TRUE)
+
+directory_and_file_structure = c("robel",
+                                 "year","month","day","observer",
+                                 "siteID","transectID","round",
+                                 "extension")
+
+robel_surveys = robel_files %>%
+  plyr::ldply(read_first_line, skip = 0,
+              into = directory_and_file_structure) %>%
+  
+  # tidyr::pivot_longer(
+  #   cols = starts_with("X"),
+  #   names_to = "nothing",
+  #   values_to = "section"
+  # ) %>%
+  # dplyr::filter(substr(section,1,1) %in% 0:10) %>%
+  
+  dplyr::mutate(date = as.Date(paste(year, month, day, sep="-"))) %>%
+  dplyr::select(date, round, transectID, observer) %>%
+  dplyr::arrange(date, transectID)
+
+
+robel_raw = robel_files %>%
+  plyr::ldply(read_robel_csv, 
+              into = directory_and_file_structure) %>%
+  
+  dplyr::mutate(filename = paste(siteID, transectID, round, sep="_"),
+                filename = paste0(filename,".",extension),
+                filepath = paste(robel, year, month, day, observer, filename, sep="/"),
+                
+                date = as.Date(paste(year, month, day, sep="-"))) %>%
+  
+  dplyr::select(-robel, -year, -month, -day, -extension, -filename) %>%
+  dplyr::select(filepath, date, round, observer, siteID, transectID, everything()) %>%
+  dplyr::arrange(date, observer, transectID)
+
+
+robel = robel_raw %>%
+  select(-filepath, -observer, -siteID) %>%
          # , -observer) %>%             
 
-  gather(distance, count,
-         -Direction, 
-         -year, -month, -day, 
-         -siteID, -transectID, -round,
-         -observer,
+  gather(section, n_bands,
+         -Direction, -date, -round, -transectID, 
          na.rm = TRUE) %>%
   
   dplyr::mutate(
     # above 16, recorded as ">16" are right censored
-    censored = ifelse(grepl(">", count), "right", "not"),
-    count    = as.numeric(gsub(">","",count)),
+    censored = ifelse(grepl(">", n_bands), "right", "not"),
+    n_bands    = as.numeric(gsub(">","",n_bands)),
     
-    # convert to cm instead of # bands
-    height = count*10,
-    
-    year  = as.numeric(year),
-    month = as.numeric(month),
-    day   = as.numeric(day)) %>% 
+    # 10 cm per band
+    height = n_bands * 10) %>% 
   
-  dplyr::rename(section = distance) %>%
-  
-  dplyr::select(year, month, day, 
-                siteID, transectID, round, section,
-                observer, Direction, height, censored)
+  dplyr::select(date, round, transectID, section, Direction, height, censored)
 	
-usethis::use_data(robel, overwrite = TRUE)
+
+usethis::use_data(robel_surveys, overwrite = TRUE)
+usethis::use_data(robel_raw,     overwrite = TRUE)
+usethis::use_data(robel,         overwrite = TRUE)
